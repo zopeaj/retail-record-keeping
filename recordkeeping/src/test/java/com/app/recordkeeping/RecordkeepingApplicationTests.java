14087ca9206ef761f78303b7281e7dac10021392
Ezekiel <zopeaj@gmail.com>
2023-05-16 06:48:27 +0100

    retail-record-keeping

---
+++ recordkeeping/src/test/java/com/app/recordkeeping/RecordkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.recordkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
